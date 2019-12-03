0e7b1fe087f327ced46ed431708a37a46016bbc8
jon7torres <jon7torres@csu.fullerton.edu>
2019-12-02 20:08:06 -0800

    Update External.hpp

--- External/External.hpp
+++ External/External.hpp
@@ -1,2 +1,18 @@
 
-class ExternalService : public  
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <stdlib.h>
+
+
+class ExternalService : public Domain::Library::SessionHandler {
+
+
+    std::vector<std::string> getCommands() override;  // retrieves the list of actions (commands)
+
+    std::vector<std::string> getSessionCommands(int selection) override;
+
+
+};
